a3d8aee830abf1376800038668714eedc04f426c
majaliju <majalijunyc@gmail.com>
2022-11-15 23:21:40 -0500

    commit tree 926cfeab9d28e60d294e9353a4d59076000ed75f

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,38 +1,46 @@
+require 'pry'
+
 class Triangle
-  attr_accessor :first, :second, :third
+## this my review attempt again to do it from scratch
+
+attr_accessor :first, :second, :third
+
+def initialize(first, second, third)
+  @first = first
+  @second = second
+  @third = third
+end
+
+def kind
+  is_a_triangle
   
-  def initialize(first, second, third)
-    @first = first
-    @second = second
-    @third = third
-  end
+  if first == second && first == third
+    :equilateral
+  elsif first == second || first == third || second == third
+    :isosceles 
+  else
+    :scalene
+   end
+end
 
-  def kind
-    validate_triangle
-
-    if first == second && second == third
-      self.kind = :equilateral
-    elsif first == second || second == third || third == first
-      self.kind = :isoceles
-    else 
-      self.kind = :scalene
+def is_a_triangle
+  if (first > 0 && second > 0 && third > 0)
+    if (first + second > third && first + third > second && second + third > first)
+      true
+    else
+      raise TriangleError
     end
-  end
-
-  def validate_triangle
-    real_triangle = [(first + second > third), (first + third > second), (second + third > first)]
-    [first, second, third].each { |s| real_triangle << false if s <= 0}
-
-    raise TriangleError if real_triangle.include?(false)
-  end
-  end
+  else
+    raise TriangleError
+    end
+end
 
 
 
 class TriangleError < StandardError
   def message
-    puts "not a triangle"
+    "This is not a triangle!"
+  end
 end
-
   
 end
